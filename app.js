561558d8a17036853b84e3e3caa76c1e5458cd25
Jackson Kelley <jackson.kelley.g@gmail.com>
2019-09-19 20:02:13 -0700

    madlib game only commit

---
+++ app.js
@@ -0,0 +1,40 @@
+
+function makeMadLib() {
+    let adjective = document.getElementById('adjective');
+    let noun = document.getElementById('noun');
+    let verbPastTense = document.getElementById('verb past-tense');
+    let adverb = document.getElementById('adverb');
+    let adjective2 = document.getElementById('adjective2');
+    let noun2 = document.getElementById('noun2');
+    let noun3 = document.getElementById('noun3');
+    let adjective3 = document.getElementById('adjective3');
+    let verb = document.getElementById('verb');
+    let adverb2 = document.getElementById('adverb2');
+    let verbPastTense2 = document.getElementById('verb past tense2');
+    let adjective4 = document.getElementById('adjective4');
+
+    let word1 = document.getElementById('word1');
+    let word2 = document.getElementById('word2');
+    let word3 = document.getElementById('word3');
+    let word4 = document.getElementById('word4');
+    const word5 = document.getElementById('word5');
+    const word6 = document.getElementById('word6');
+    const word7 = document.getElementById('word7');
+    const word8 = document.getElementById('word8');
+    const word9 = document.getElementById('word9');
+    const word10 = document.getElementById('word10');
+    const word11 = document.getElementById('word11');
+    const word12 = document.getElementById('word12');
+    word1.textContent = adjective.value;
+    word2.textContent = noun.value;
+    word3.textContent = verbPastTense.value;
+    word4.textContent = adverb.value;
+    word5.textContent = adjective2.value;
+    word6.textContent = noun2.value;
+    word7.textContent = noun3.value;
+    word8.textContent = adjective3.value;
+    word9.textContent = verb.value;
+    word10.textContent = adverb2.value;
+    word11.textContent = verbPastTense2.value;
+    word12.textContent = adjective4.value;
+}
